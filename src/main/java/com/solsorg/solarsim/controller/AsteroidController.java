d6622016d20f10fd554cfd0bce9e1025856a4058
Saumil Sharma <109639693+Galagyy@users.noreply.github.com>
2024-08-29 09:45:39 -0700

    Asteroid Update
    Added the Asteroid class as well as the proper Asteroid controller, service, and repository.

---
+++ src/main/java/com/solsorg/solarsim/controller/AsteroidController.java
@@ -0,0 +1,60 @@
+package com.solsorg.solarsim.controller;
+
+import com.solsorg.solarsim.model.Asteroid;
+import com.solsorg.solarsim.service.AsteroidService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asteroids")
+public class AsteroidController {
+    @Autowired
+    private AsteroidService asteroidService;
+
+    @GetMapping
+    public ResponseEntity<List<Asteroid>> getAllAsteroids(){
+        List<Asteroid> asteroids = asteroidService.getAll();
+
+        if(asteroids.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(asteroids, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Asteroid> getFromID(@PathVariable long id){
+        Asteroid found = asteroidService.getFromID(id);
+
+        if(found == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(found, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Asteroid> createAsteroid(@RequestBody Asteroid asteroid){
+        if(asteroidService.saveAsteroid((asteroid)) != null){
+            return new ResponseEntity<>(asteroid, HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteAsteroid(@PathVariable long id){
+        Asteroid found = asteroidService.getFromID(id);
+
+        if(found == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        asteroidService.deleteAsteroid(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/solsorg/solarsim/service/AsteroidService.java
@@ -0,0 +1,30 @@
+package com.solsorg.solarsim.service;
+
+import com.solsorg.solarsim.model.Asteroid;
+import com.solsorg.solarsim.repository.AsteroidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AsteroidService {
+    @Autowired
+    private AsteroidRepository asteroidRepository;
+
+    public List<Asteroid> getAll(){
+        return asteroidRepository.findAll();
+    }
+
+    public Asteroid getFromID(long id){
+        return asteroidRepository.findById(id).orElse(null);
+    }
+
+    public Asteroid saveAsteroid(Asteroid asteroid){
+        return asteroidRepository.save(asteroid);
+    }
+
+    public boolean deleteAsteroid(long id){
+        asteroidRepository.deleteById(id);
+    }
+}
