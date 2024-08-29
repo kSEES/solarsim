0299504d6a56b7123db027f1ccb8547b09fc380c
Saumil Sharma <109639693+Galagyy@users.noreply.github.com>
2024-08-29 13:20:46 -0700

    Removing Deprecated Objects
    Removal of deprecated items such as DTOs and ScientificNotation.

--- src/main/java/com/solsorg/solarsim/SolarsimApplication.java
+++ src/main/java/com/solsorg/solarsim/SolarsimApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SolarsimApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SolarsimApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/solsorg/solarsim/util/math/SpaceFunction.java
@@ -0,0 +1,9 @@
+package com.solsorg.solarsim.util.math;
+
+public class SpaceFunction {
+    private String function;
+
+    public static void solveFunction(SpaceFunction function, double x){
+        
+    }
+}
