44051d37a068969402277b3b8ed230ef24890bb3
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-08-31 21:53:23 -0700

    Favicon Addition + script.js Reformat
    Reformatted script.js to have proper formatting as well as added the favicon configuration in order to make springboot properly display the favicon.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () { //Establishing JS to run after HTML DOM Content is Loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const astrodata = document.querySelectorAll(".astro_data");
 
-    const astrodata = document.querySelectorAll(".astro_data"); //Established an "astrodata" constant that will hold everything in the class (named with a placeholder) astro_data.
-    
-    astrodata.forEach((planetbutton) => { //Established a function where each "planetbutton" will do something relating to "astrodata."
-
-        planetbutton.addEventListener("mouseclick",  () => { //Hopefully established something where the "planetbutton" will look for clicks and perform and action.
-            
-            fetch(".attributes"); //Hopefully established a function that fetches information with the class "attributes"
-        });
+  astrodata.forEach((planetbutton) => {
+    planetbutton.addEventListener("mouseclick", () => {
+      fetch(".attributes");
     });
+  });
 });
+
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    if (menu.style.right === '0px') {
-        menu.style.right = '-250px';
-    } else {
-        menu.style.right = '0px';
-    }
+  const menu = document.querySelector(".menu");
+  if (menu.style.right === "0px") {
+    menu.style.right = "-250px";
+  } else {
+    menu.style.right = "0px";
   }
+}
