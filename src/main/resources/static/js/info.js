9a207a55c17cac436b5876b6dd327feca6d2ef68
Kason Lai <kason_lai@yahoo.com>
2024-09-02 11:31:56 -0700

    Info Edits
    Edited Info for some more frontend-backend testing.

--- src/main/resources/static/js/info.js
+++ src/main/resources/static/js/info.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+<<<<<<< Updated upstream
   async function planetdatafetch() {
     const jsonData = await fetch("/api/planets", {
       method: "GET",
@@ -7,6 +8,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const response = await response.json();
   }
   planetdatafetch();
+=======
+    var response;
+    async function planetdatafetch() { //Attempted Async Function, but not sure if the never being called thing will work.
+        const jsonData = await fetch("/api/planets", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    });
+    response = await jsonData.json();  
+    }
+
+    planetdatafetch();
+    
+        var planetinfo = response;
+        var mercuryinfo = planetinfo[0];
+        var venusinfo = planetinfo[1];
+        var earthinfo = planetinfo[2];
+        var marsinfo = planetinfo[3];
+        var jupiterinfo = planetinfo[4];
+        var saturninfo = planetinfo[5];
+        var uranusinfo = planetinfo[6];
+        var neptuneinfo = planetinfo[7];
+        
+>>>>>>> Stashed changes
 
   var planetinfo = response.json();
   var mercuryinfo = planetinfo[0];
