606dc51fc33c602d64975c90b41af5cc5d3952c0
Kason Lai <kason_lai@yahoo.com>
2024-09-01 23:16:49 -0700

    Fixing and Linking in Planets
    Fixed a merge conflict and added some JS elements in planets.html to start establishing information fetching.

--- src/main/resources/static/js/info.js
+++ src/main/resources/static/js/info.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    const planetdata = 
+    
+    fetch('/api/planets', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        });
+        
 });
