fb6d8700be236b42eed1bd7cd8d5362615ca99e6
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-09-06 20:31:21 -0700

    Asteroid.java Update
    Updated Asteroid.java to have proper getters, setters, and constructors.

--- src/main/java/com/solsorg/solarsim/model/Asteroid.java
+++ src/main/java/com/solsorg/solarsim/model/Asteroid.java
@@ -5,5 +5,50 @@ import jakarta.persistence.Entity;
 @Entity
 public class Asteroid extends SpaceEntity{
     private String name;
-    
+    private String composition;
+    private double size;
+
+    public Asteroid(double x, double y, double z, double mass, String name, String composition, double size){
+        super(x, y, z, mass);
+
+        this.name = name;
+        this.composition = composition;
+        this.size = size;
+    }
+
+    public Asteroid(String name, String composition, double size) {
+        this.name = name;
+        this.composition = composition;
+        this.size = size;
+    }
+
+    public Asteroid(){
+        this.name = "Undefined";
+        this.composition = "Undefined";
+        this.size = -1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComposition() {
+        return composition;
+    }
+
+    public void setComposition(String composition) {
+        this.composition = composition;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
 }
