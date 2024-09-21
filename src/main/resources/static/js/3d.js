a8ae0201685c13590b57bb934c11c23f3dc7edb9
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-09-21 13:20:14 -0700

    3D.js Comment Update
    Updated 3D.js to have proper comments and be formatted better.

--- src/main/resources/static/js/3d.js
+++ src/main/resources/static/js/3d.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
-// Imported three.js
+
+/*
+    Creation of camera and scenes to prepare OpenGL for programming.
+    Created a new camera with a perspective in the middle of a window.
+ */
 const scene = new THREE.Scene();
-// Set up a scene using the three.js constructor.
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// Set up a camera using the three.js constructor.
 const renderer = new THREE.WebGLRenderer();
-// Set up a renderer using the three.js constuctor.
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-// Set the size of the renderer based on the dimensions of the camera.
 document.body.appendChild(renderer.domElement);
